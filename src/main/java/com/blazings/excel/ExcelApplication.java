c78910c2339adf386c8b9a37ea86e99b21f25242
JBlazingSun <blazings@vip.qq.com>
2020-09-18 15:37:39 +0800

    add

--- src/main/java/com/blazings/excel/ExcelApplication.java
+++ src/main/java/com/blazings/excel/ExcelApplication.java
@@ -1,6 +1,7 @@
 package com.blazings.excel;
 
 import Model.DistriData;
+import cn.hutool.core.io.FileUtil;
 import com.alibaba.excel.EasyExcel;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -42,26 +43,89 @@ public class ExcelApplication implements CommandLineRunner {
     }
     @Override
     public void run(String... args) throws Exception {
-        disableAccessWarnings();
-        String sheetNameInput = "总表";
+//        disableAccessWarnings();
+        //
+        List<DistriData> distriDataOut = new ArrayList<>();
+        //只有名字和电话
+        List<DistriData> distriDataOnlyNameAndPhone = new ArrayList<>();
+        //名字和电话都是空
+        List<DistriData> distriDataNoMsg = new ArrayList<>();
+
+        String sheetNameInput = "t";
 //        Scanner scan = new Scanner(System.in);
 //        sheetNameInput = scan.nextLine();
 //        scan.close();
 
-        String fileName = "C:\\blazings\\download\\9-15到9-17-10.40-唯一.xlsx";
-        String templateFileName = "C:\\blazings\\download\\德邦快递精简模板列表.xlsx";
-        String fillFileName = "C:\\blazings\\download\\null.xlsx";
-        List<DistriData> distriDataOut = new ArrayList<>();
+        //C:\blazings\同步\work\易拼\物流\发货
+        String sourceFileName = "C:\\blazings\\同步\\work\\易拼\\物流\\发货\\9-15到9-17-10.40多次辟谷丹.xlsx";
+        //C:\blazings\同步\work\易拼\物流\打单
+        String templateFileName = "C:\\blazings\\同步\\work\\易拼\\物流\\打单\\德邦快递精简模板列表.xlsx";
+        //C:\blazings\同步\work\易拼\物流\打单
+        //C:\blazings\download
+        String fillFileName = "C:\\blazings\\同步\\work\\易拼\\物流\\打单\\null.xlsx";
+        String fillDestFileName = "C:\\blazings\\download\\德邦上传.xlsx";
+        String fillDestFileNameOnlyNameAndPhone = "C:\\blazings\\download\\德邦上传没有地址-只有电话和姓名.xlsx";
+        String fillDestFileNameNoMsg = "C:\\blazings\\download\\德邦上传没有任何信息.xlsx";
+        FileUtil.copy(fillFileName,fillDestFileName,true);
 
-        ExcelDataListener dataListener = new ExcelDataListener();
+        ExcelDataListener excelDataListener = new ExcelDataListener();
         // 这里 需要指定读用哪个class去读，然后读取第一个sheet 文件流会自动关闭
-        EasyExcel.read(fileName, DistriData.class, dataListener).sheet(sheetNameInput).doRead();
-//        for (DistriData distriData : dataListener.list) {
-//            for (DistriData data : distriDataOut) {
-//
-//            }
-//        }
+        EasyExcel.read(sourceFileName, DistriData.class, excelDataListener).sheet(sheetNameInput).doRead();
 
-        EasyExcel.write(fillFileName).withTemplate(templateFileName).sheet().doFill(dataListener.list);
+        for (DistriData excelSourceData : excelDataListener.list) {
+            //如果名字和电话都是空
+            if (excelSourceData.getConsignee()==null &&
+                    excelSourceData.getPhone()==null){
+                distriDataNoMsg.add(excelSourceData);
+                continue;
+            }
+            //如果只有名字和电话
+            if (excelSourceData.getConsignee()!=null &&
+                    excelSourceData.getPhone()!=null &&
+                    excelSourceData.getAddress()==null){
+                distriDataOnlyNameAndPhone.add(excelSourceData);
+                continue;
+            }
+            //正常收货
+            //
+            if (distriDataOut.stream().count()>0){
+                for (DistriData dataOut : distriDataOut) {
+                    if (excelSourceData.getConsignee()!=null &&
+                            excelSourceData.getPhone()!=null &&
+                            excelSourceData.getAddress()!=null){
+                        distriDataOut.add(excelSourceData);
+                        break;
+                    }
+                }
+            }
+            //第一次对比
+            else {
+                if (excelSourceData.getConsignee()!=null &&
+                        excelSourceData.getPhone()!=null &&
+                        excelSourceData.getAddress()!=null){
+                    distriDataOut.add(excelSourceData);
+                }
+            }
+        }
+
+        //打印空白表
+        if (distriDataNoMsg.stream().count()>0){
+            FileUtil.copy(fillFileName,fillDestFileNameNoMsg,true);
+            for (DistriData distriDataNoMsgAddOrderId : distriDataNoMsg) {
+                distriDataNoMsgAddOrderId.setRemark(distriDataNoMsgAddOrderId.getOrderID());
+            }
+            EasyExcel.write(fillDestFileNameNoMsg).withTemplate(templateFileName).sheet().doFill(distriDataNoMsg);
+
+        }
+        //打印名字和电话都是空
+        if (distriDataOnlyNameAndPhone.stream().count()>0){
+            FileUtil.copy(fillFileName,fillDestFileNameOnlyNameAndPhone,true);
+            for (DistriData distriDataOnlyNameAndPhoneAddId : distriDataOnlyNameAndPhone) {
+                distriDataOnlyNameAndPhoneAddId.setRemark(distriDataOnlyNameAndPhoneAddId.getOrderID());
+            }
+            EasyExcel.write(fillDestFileNameOnlyNameAndPhone).withTemplate(templateFileName).sheet().doFill(distriDataOnlyNameAndPhone);
+
+        }
+        EasyExcel.write(fillDestFileName).withTemplate(templateFileName).sheet().doFill(distriDataOut);
     }
 }
--- src/main/java/com/blazings/excel/ExcelDataListener.java
+++ src/main/java/com/blazings/excel/ExcelDataListener.java
@@ -10,24 +10,21 @@ import java.util.Date;
 import java.util.List;
 
 public class ExcelDataListener extends AnalysisEventListener<DistriData> {
-    /**
-     * 每隔5条存储数据库，实际使用中可以3000条，然后清理list ，方便内存回收
-     */
-    private static final int BATCH_COUNT = 5;
+
     List<DistriData> list = new ArrayList<DistriData>();
     public ExcelDataListener() {
-        // 这里是demo，所以随便new一个。实际使用如果到了spring,请使用下面的有参构造函数
     }
 
     @Override
     public void invoke(DistriData distriData, AnalysisContext analysisContext) {
         //临时唯一用, 最后处理
-        distriData.setRemark(distriData.getGoodsName()+"   数量: "+distriData.getGoodsCount()+"   价格:"+Integer.valueOf(distriData.getGoodsCount())* 399);
+//        distriData.setRemark(distriData.getGoodsName()+"   数量: "+distriData.getGoodsCount()+"   价格:"+Integer.valueOf(distriData.getGoodsCount())* 399);
+        distriData.setGoodsCount("1");
 
         //固定
-//        distriData.setTransportProperties("标准快递");
-//        distriData.setPayType("月结");
-//        distriData.setDeliveryType("送货上楼");
+        distriData.setTransportProperties("标准快递");
+        distriData.setPayType("月结");
+        distriData.setDeliveryType("送货上楼");
         list.add(distriData);
     }
 
